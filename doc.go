30c6d5949f3c2c498ac7baf45190aaaa0a40a6c0
facebadge <facebadge@mobilana.mobi>
2020-09-14 21:03:35 +0300

    add doc into

---
+++ doc.go
@@ -0,0 +1,28 @@
+/*
+
+Package sdk is used to develop **pure functional** and **typesafe** Behavior as
+a Code to validate applications. https://assay.it automates the validation
+process along CI/CD pipelines. Continuous proofs of the quality helps to
+eliminate defects at earlier phases of loosely coupled topologies such as
+serverless applications, microservices and other systems that rely on interface
+syntaxes and its behaviors.
+
+
+Further Reading
+
+https://github.com/assay-it/sdk-go
+SDK Source Code
+
+https://github.com/assay-it/sample.assay.it
+Minimal example is an annotated walk-through and a minimal Behavior as a Code
+suite. Use it as getting started tutorial.
+
+https://github.com/assay-it/example.assay.it
+Advanced example os an annotated walk-through about CI/CD workflows, shows
+advanced usage of Behavior as a Code paradigm.
+
+https://assay.it/doc/core
+The documentation is a general introduction on how to configure and use the SDK.
+
+*/
+package sdk
